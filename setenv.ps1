3d00648d8455c896e7f8e7501c9aefc78c0b0d08
michaeljqzq <michaeljqzq@gmail.com>
2018-04-10 16:17:06 +0800

    remove unused files

--- setenv.ps1
+++
@@ -1,3 +0,0 @@
-$env:INPUT_containerregistrytype = "Container Registry"
-$env:INPUT_dockerFile = "C:/Users/v-zhq/IoT/edge/newsolution/newsolution/modules/SampleModule/Dockerfile"
-$env:BUILD_SOURCESDIRECTORY = "C:/Users/v-zhq/IoT/edge/newsolution/newsolution"
